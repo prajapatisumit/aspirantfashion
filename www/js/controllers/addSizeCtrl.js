f351e60bc3e5a353da2d1ce97359e7648a2c87aa
Sumit Prajapati <prajapatisumit2006@gmail.com>
2017-07-25 17:20:48 +0530

    create a new module add size

---
+++ www/js/controllers/addSizeCtrl.js
@@ -0,0 +1,25 @@
+angular.module('app')
+.controller('addSizeCtrl', function($scope,$rootScope,sharedUtils,$ionicSideMenuDelegate,$interval,
+                                     $state,fireBaseData,$ionicHistory,SessionService,$ionicModal,$firebaseArray,$firebaseObject,$stateParams,CommonService,IonicPopupService) {
+
+    $rootScope.extras=true;
+
+    $scope.addshippingrate = function (shippingInguj,shippingOutguj) {
+        var shippingObj = {
+            guajrat : shippingInguj,
+            outofguajrat : shippingOutguj
+        }
+
+      var ShippingRef = firebase.database().ref().child('shipping rate').push(shippingObj).key;
+      console.log("shippingObj : " + angular.toJson(shippingObj , ' '));
+      // $scope.globalproductID = BrandRef;
+      // console.log("$scope.globalcategory "+ $scope.globalproductID);
+      // if (!!$scope.globalproductID) {
+      //   var imgObj = {
+      //     image : $scope.downloadURL
+      //   };
+      //  firebase.database().ref().child('brand/' + $scope.globalproductID + '/images' ).set($scope.imgset3);
+      // }
+    };
+
+})
